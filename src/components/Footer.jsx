1c4f7dc57d0de5abd0320d83ff7255cc5d8d0101
poursha98 <ghostworrior8@gmail.com>
2023-06-05 14:14:32 +0330

    footer styled, waiting for links and data

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,22 @@
 import InstagramIcon from '@mui/icons-material/Instagram'
 import TelegramIcon from '@mui/icons-material/Telegram';
+import CallIcon from '@mui/icons-material/Call';
+
 function Footer() {
     return (
-        <footer className='bg-dark text-light pt-5 pb-4'>
-            <div className='container text-center text-md-start'>
+        <footer className='bg-dark text-light pt-5 pb-4 fixed-bottom'>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col'>Contact me</div>
+                    <div className='cok'>Email</div>
+                    <div className='cok'>Privacy</div>
+                </div>
+
                 <div className='d-flex justify-content-center text-md-start'>
-            <div><InstagramIcon/></div>
-            <div><TelegramIcon/></div>
+                    <div><InstagramIcon/></div>
+                    <div><TelegramIcon/></div>
+                    <div><CallIcon/></div>
+
                 </div>
             </div>
         </footer>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,28 +1,32 @@
-import {Navbar,Nav} from "react-bootstrap";
+import {Navbar, Nav} from "react-bootstrap";
 
 function Navigation() {
     return (
         <div>
-        <Navbar bg='dark' variant='dark' sticky='top' expand='sm' >
-            <Navbar.Brand className='m-2'>
-                LOGO
-            </Navbar.Brand>
-            <Navbar.Toggle className='me-4  '/>
-            <Navbar.Collapse>
-                <Nav className='mx-4 ms-2'>
-                    <Nav.Link href='/' className='opacity-100 link-opacity-75-hover link-light text-decoration-none'>
-                            Home
-                    </Nav.Link>
-                    <Nav.Link  href='/projects' className='opacity-100 link-opacity-75-hover link-light text-decoration-none'>
-                        Projects
-                    </Nav.Link>
-                    <Nav.Link href='/experience' className='opacity-100 link-opacity-75-hover link-light text-decoration-none'>
-                        Experience
-                    </Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-
-        </Navbar>
+            <Navbar bg='dark' variant='dark' sticky='top' expand='sm'>
+                <div className='container'>
+                    <Navbar.Brand className='m-1'>
+                        LOGO
+                    </Navbar.Brand>
+                    <Navbar.Toggle className='me-4  '/>
+                    <Navbar.Collapse>
+                        <Nav className='mx-4 ms-2'>
+                            <Nav.Link href='/'
+                                      className='opacity-100 link-opacity-75-hover link-light text-decoration-none'>
+                                Home
+                            </Nav.Link>
+                            <Nav.Link href='/projects'
+                                      className='opacity-100 link-opacity-75-hover link-light text-decoration-none'>
+                                Projects
+                            </Nav.Link>
+                            <Nav.Link href='/experience'
+                                      className='opacity-100 link-opacity-75-hover link-light text-decoration-none'>
+                                Experience
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </div>
+            </Navbar>
         </div>
 
     )
