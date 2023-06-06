fa3da15284555c8e825f90ec308aa6be6461183f
poursha98 <ghostworrior8@gmail.com>
2023-06-06 17:22:30 +0330

    homepage intro section vh100 implemented and styled

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,25 +4,26 @@ function Home() {
     return (
         <div className='home'>
 
-            <div className='about d-flex flex-column text-center container container-lg p-5 mx-0 mw-100'>
-                <h1 className='fw-bold text-light my-4'>Hi, My Name is MyName</h1>
-                <div className='prompt text-warning m-2'>
+            <div className='about d-flex flex-column text-center container container-lg p-5 mx-0 mw-100 vh-100 align-items-center justify-content-center'>
+                <h1 className='fw-bold mb-5 text-warning '>Hi, My Name is MyName</h1>
+                <div className='prompt m-2 mt-5 text-light'>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.\n
                         Ad adipisci aliquam aut 😊</p>
                 </div>
             </div>
-            <div className='skills vh-100 my-5'>
+            <div className='skills vh-100 my-5 mx-4'>
                 <ol className='list d-flex flex-column justify-content-center align-items-center text-center'>
+                    <li className='h1 fw-bold list-group-item mb-5'>Skills</li>
                     <li className='items list-group-item my-4'>
-                        <h2 className='fw-bold '>Front-End</h2>
+                        <h2 className='fw-bold'>Front-End</h2>
                         <span>HTML, CSS, Bootstrap, Tailwind, MUI, Javascript, React.Js , React Router, Redux, Next.JS, RestAPI</span>
                     </li>
                     <li className='items list-group-item my-4'>
-                        <h2>Back-End</h2>
+                        <h2 className='fw-bold'>Back-End</h2>
                         <span>Node.JS, Express.JS, PostgreSQL </span>
                     </li>
                     <li className='items list-group-item my-4'>
-                        <h2>Languages</h2>
+                        <h2 className='fw-bold'>Languages</h2>
                         <span>English, French</span>
                     </li>
                 </ol>
