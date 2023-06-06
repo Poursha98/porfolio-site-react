7dc0a08f5bcb6fad3981c9933bac277073176547
poursha98 <ghostworrior8@gmail.com>
2023-06-06 23:08:53 +0330

    nav reafactor for better performance and some vertical timeline styles in progress

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,26 +1,31 @@
 import {Navbar, Nav} from "react-bootstrap";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function Navigation() {
-    const [nav,setNav] = useState(false)
+    const [windowScrollCheck, setWindowScrollCheck] = useState(false)
+    useEffect(() => {
+        const changeBg = () => {
+            if (window.scrollY >= 56) {
+                setWindowScrollCheck(true)
+            } else {
+                setWindowScrollCheck(false)
+            }
 
-    const changeBg = ()=>{
-        if(window.scrollY >=56){
-            setNav(true)
-        } else {
-            setNav(false)
         }
-    }
-    // window.addEventListener('scroll',changeBg)
+        return window.addEventListener('scroll', changeBg);
+
+    },[])
+
+
     return (
         <div className='sticky-top'>
-            <Navbar className={nav ? 'myNav active' : 'myNav'} variant='dark' sticky='top' expand='sm' >
+            <Navbar className={windowScrollCheck ? 'myNav active' : 'myNav'} variant='dark' sticky='top' expand='sm'>
                 <div className='container m-0'>
                     <Navbar.Brand className='m-1'>
                         LOGO
                     </Navbar.Brand>
-                    <Navbar.Toggle className='me-4' />
-                    <Navbar.Collapse >
+                    <Navbar.Toggle className='me-4'/>
+                    <Navbar.Collapse>
                         <Nav className='mx-4 ms-2'>
                             <Nav.Link href='/'
                                       className='opacity-100 link-opacity-75-hover link-light text-decoration-none'>
--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,7 +1,38 @@
-
+import {VerticalTimeline,VerticalTimelineElement} from 'react-vertical-timeline-component'
+import 'react-vertical-timeline-component/style.min.css'
+import SchoolIcon from '@mui/icons-material/School'
+// import GroupIcon from '@mui/icons-material/Group';
 function Experience() {
     return (
-        <div>Experience</div>
+        <div className='experience container'>
+            <VerticalTimeline lineColor='#3e497a'>
+                <VerticalTimelineElement className='vertical-timeline-element--education'
+                date='1/1/1 to 3/3/3' iconStyle={{background:'#3e497a',color:'#fff'}}
+                icon={<SchoolIcon/>}>
+                    <h3 className='vertical-timeline-element-title'>Your University</h3>
+                    {/* eslint-disable-next-line react/no-unescaped-entities */}
+                    <p>Bachelor's degree for example</p>
+                </VerticalTimelineElement>
+            </VerticalTimeline>
+            <VerticalTimeline lineColor='#3e497a'>
+                <VerticalTimelineElement className='vertical-timeline-element--education'
+                                         date='3/3/3 to 5/5/5' iconStyle={{background:'#3e497a',color:'#fff'}}
+                                         icon={<SchoolIcon/>}>
+                    <h3 className='vertical-timeline-element-title'>Your Other University</h3>
+                    {/* eslint-disable-next-line react/no-unescaped-entities */}
+                    <p>Master's degree for example</p>
+                </VerticalTimelineElement>
+            </VerticalTimeline>
+            <VerticalTimeline lineColor='#3e497a'>
+                <VerticalTimelineElement className='vertical-timeline-element--education'
+                                         date='1/1/1 to 3/3/3' iconStyle={{background:'#0BB016FF', color:'#fff'}}
+                                         icon={<SchoolIcon/>}>
+                    <h3 className='vertical-timeline-element-title'>Your University</h3>
+                    {/* eslint-disable-next-line react/no-unescaped-entities */}
+                    <p>Master's degree for example</p>
+                </VerticalTimelineElement>
+            </VerticalTimeline>
+        </div>
     )
 }
 
