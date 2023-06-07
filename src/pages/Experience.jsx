75a7d7359675b8b78c64ef6c4ec5b0e8b5b7df27
poursha98 <ghostworrior8@gmail.com>
2023-06-07 09:15:37 +0330

    dependency update, cache clean up , experience section styled

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,7 +1,7 @@
 import {VerticalTimeline,VerticalTimelineElement} from 'react-vertical-timeline-component'
 import 'react-vertical-timeline-component/style.min.css'
 import SchoolIcon from '@mui/icons-material/School'
-// import GroupIcon from '@mui/icons-material/Group';
+import GroupIcon from '@mui/icons-material/Group';
 function Experience() {
     return (
         <div className='experience container'>
@@ -25,11 +25,20 @@ function Experience() {
             </VerticalTimeline>
             <VerticalTimeline lineColor='#3e497a'>
                 <VerticalTimelineElement className='vertical-timeline-element--education'
-                                         date='1/1/1 to 3/3/3' iconStyle={{background:'#0BB016FF', color:'#fff'}}
-                                         icon={<SchoolIcon/>}>
-                    <h3 className='vertical-timeline-element-title'>Your University</h3>
+                                         date='5/5/5 to 6/6/6' iconStyle={{background:'#4dd4e3', color:'#fff'}}
+                                         icon={<GroupIcon/>}>
+                    <h3 className='vertical-timeline-element-title'>Your First Job Experience</h3>
                     {/* eslint-disable-next-line react/no-unescaped-entities */}
-                    <p>Master's degree for example</p>
+                    <p>junior worker</p>
+                </VerticalTimelineElement>
+            </VerticalTimeline>
+            <VerticalTimeline lineColor='#3e497a'>
+                <VerticalTimelineElement className='vertical-timeline-element--education'
+                                         date='6/6/6 to present' iconStyle={{background:'#4dd4e3', color:'#fff'}}
+                                         icon={<GroupIcon/>}>
+                    <h3 className='vertical-timeline-element-title'>Your current Job Experience</h3>
+                    {/* eslint-disable-next-line react/no-unescaped-entities */}
+                    <p>senior worker</p>
                 </VerticalTimelineElement>
             </VerticalTimeline>
         </div>
